244da86d07079c9bc4bd72cc72831414d6064f7a
Geoyeob Kim <gasbank@gmail.com>
2020-12-22 06:13:05 +0900

    Client-side platform receipt verifier added.

---
+++ client/PlatformReceiptVerifier.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class PlatformReceiptVerifier {
+    static readonly string VERIFIER_URL = "http://localhost/verifyGooglePlay";
+
+    static string Base64Encode(string plainText) {
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+        return Convert.ToBase64String(plainTextBytes);
+    }
+
+    public static async Task<bool> Verify(string receipt) {
+        try {
+            using (var httpClient = new HttpClient()) {
+                httpClient.DefaultRequestHeaders.Add("receipt-to-be-verified", Base64Encode(receipt));
+                var getTask = await httpClient.GetAsync(new Uri(VERIFIER_URL));
+                if (getTask.IsSuccessStatusCode) {
+                    Debug.Log("Receipt verification: OK");
+                    return true;
+                }
+
+                if (Application.isEditor) {
+                    Debug.Log($"Receipt verification: failed. status code={getTask.StatusCode} (ALWAYS FAIL IN EDITOR MODE. NOT AN ERROR)");
+                    return true;
+                }
+
+                Debug.LogError($"Receipt verification: failed. status code={getTask.StatusCode}");
+            }
+        } catch (Exception e) {
+            Debug.LogError($"Receipt verification: failed with exception. {e}");
+        }
+        return false;
+    }
+}
